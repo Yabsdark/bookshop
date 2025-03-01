45e6c0675a17267a18c619fab1fc4535d742cc2c
Yigael <gatodolyb@gmail.com>
2025-03-01 15:36:27 -0600

    primer commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
+import React from 'react';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-export default function App() {
-  return (
+const HomeScreen: React.FC = () => (
+  <Layout style={styles.container}>
+    <Text category='h1'>HOME</Text>
+  </Layout>
+);
+
+const App: React.FC = () => (
+  <ApplicationProvider {...eva} theme={eva.light}>
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <HomeScreen />
       <StatusBar style="auto" />
     </View>
-  );
-}
+  </ApplicationProvider>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
   },
 });
+
+export default App;
